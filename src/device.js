e163bd51f2b267742a5b31a6baa0ccd5d5a6e3e3
Kimberly Swinton <kimberlyswinton@gmail.com>
2021-05-15 21:40:17 -0400

    Added every breakpoint in Chome dev tools into device.js

--- src/device.js
+++ src/device.js
@@ -1,27 +1,31 @@
 
 
 const size = {
-    mobileS: '320px',
-    mobileM: '375px',
-    mobileL: '425px',
-    tabletS: '481px',
-    tablet: '768px',
-    laptop: '1024px',
-    laptopL: '1440px',
-    desktop: '2560px'
+    galaxyFold: '512px',
+    surfaceDuo: '540px',
+    iPadPro: '1024px',
+    iPad: '768px',
+    iPhoneX: '375px',
+    iPhone678Plus: '414px',
+    iPhone678: '375px',
+    iPhone5SE: '2560px',
+    pixel2XL: '411px',
+    galaxyS5: '360px',
+    
   }
 
 
 export const device = {
-    mobileS: `(max-width: ${size.mobileS})`,
-    mobileM: `(max-width: ${size.mobileM})`,
-    mobileL: `(max-width: ${size.mobileL})`,
-    tabletS: `(max-width: ${size.tabletS})`,
-    tablet: `(max-width: ${size.tablet})`,
-    laptop: `(max-width: ${size.laptop})`,
-    laptopL: `(max-width: ${size.laptopL})`,
-    desktop: `(max-width: ${size.desktop})`,
-    desktopL: `(max-width: ${size.desktop})`
+    galaxyFold: `(max-width: ${size.galaxyFold})`,
+    surfaceDuo: `(max-width: ${size.surfaceDuo})`,
+    iPadPro: `(max-width: ${size.iPadPro})`,
+    iPad: `(max-width: ${size.iPad})`,
+    iPhoneX: `(max-width: ${size.iPhoneX})`,
+    iPhone678Plus: `(max-width: ${size.iPhone678Plus})`,
+    iPhone678: `(max-width: ${size.iPhone678})`,
+    iPhone5SE: `(max-width: ${size.iPhone5SE})`,
+    pixel2XL: `(max-width: ${size.pixel2XL})`,
+
   };
 
 
