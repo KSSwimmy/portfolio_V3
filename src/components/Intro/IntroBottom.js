250b9a9ebb1e407bb8d81a6eb5bc62404d00796b
Kimberly Swinton <kimberlyswinton@gmail.com>
2021-06-08 20:13:29 -0400

    Minor change to intoduction

--- src/components/Intro/IntroBottom.js
+++ src/components/Intro/IntroBottom.js
@@ -18,7 +18,7 @@ const IntroBottom = () => {
 
                <JobTitleTwo> 
 
-               I am a UX Designer and Software Engineer. I built and designed this site from scratch! With that being said, you can also call me a UX Engineer. 
+               I am a UX Designer and Software Engineer. I designed and built this site from scratch! With that being said, you can also call me a UX/UI Engineer. 
                 
                 </JobTitleTwo> 
             </JobTitleContainer>
