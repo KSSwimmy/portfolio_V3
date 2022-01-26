d75e52bad95b19d757a0a6cfe314dea0ead243e7
Kimberly Swinton <kimberlyswinton@gmail.com>
2022-01-26 12:55:06 -0500

    centered about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,7 +16,7 @@ const AboutMe = () => {
                     <About>ABOUT</About>
                 </AboutBorderDiv>
 
-                <TheHTwo>Hi! It's Me! Kimberly!</TheHTwo>
+                <TheHTwo>HI! IT'S ME! KIMBERLY!</TheHTwo>
                 <FirstP>
 
                     <p>I’m a Full Stack Software Engineer with design being my first love. I was born and raised in the PeeDee area of South Carolina, but the beat of a fast paced “Concrete Jungle” called, New York City caught my heart! I have a flair for problem solving complex problems from code to design. Being able to empathize with the user is my top priority as a designer, software engineer, and also as a person who is visually impaired. Diversity and inclusion is needed in the tech industry because it serves a plethora of people who are not the same. I feel I'd be a perfect fit on any team because of my passion to make technology more beautifully designed and accessible for all! 
@@ -89,7 +89,7 @@ color: #DCE3FF;
 margin-top: 40px;
 margin-bottom: 40px;
 font-size: 2.5rem ;
-text-align: left;
+text-align: center;
 `
 
 const ThirdP = styled.div`
